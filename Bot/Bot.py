e056ce5c30d5018e39254807c690ea4b9387d63b
IldarAkhm <112767933+IldarAkhm@users.noreply.github.com>
2024-02-12 00:46:04 +0300

    change files

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,21 +1,22 @@
 from aiogram import Bot, Dispatcher, executor, types
 from TOKEN import TOKEN_API
-from model import get_model
-import Vocabulary
+from model import get_model, TextCNN
+from Vocabulary import Vocabulary
 import torch
 from database import engine
 import pandas as pd
 
 
+
 bot = Bot(TOKEN_API) # создаем бота с нашим токеном
 dp = Dispatcher(bot) # анализ и инициализая всех входящих апдейтов, функциональность нашего бота
 
-model = get_model() # инициализируем нашу модель
+model = get_model().to('cpu') # инициализируем нашу модель
 
 texts = pd.read_sql(
     """
         SELECT *
-        FROM public.email_texts
+        FROM public.data_for_voc
     """,
     con=engine
 )['text'] # берем из наших данных только тексты
@@ -30,8 +31,13 @@ pad_idx = len(voc.vocabulary) # длина словаря
 @dp.message_handler(commands=['start'])
 async def echo(message: types.Message):
     text_list = voc.encode(message.text)
-    for_model = torch.tensor(text_list + [pad_idx] * (1043 - len(text_list)))
-    await message.answer(text='Иди нахуй, Рамис')
+    for_model = torch.tensor(text_list + [pad_idx] * (4 - len(text_list))).unsqueeze(0) # максимальное ядро свертки 4
+    answer = int(torch.round(torch.sigmoid(model(for_model)[0][0])))
+
+    if answer:
+        await message.answer(text='Spam')
+    else:
+        await message.answer(text='Ham')
 
 
 if __name__ == '__main__':
--- Bot/model.py
+++ Bot/model.py
@@ -1,7 +1,57 @@
 import torch
+import torch.nn.functional as F
+import torch.nn as nn
 
 
 def get_model():
     model = torch.load('TextCNN/textcnn_spamdestroyer.pth')
 
     return model
+
+
+class TextCNN(nn.Module):
+    def __init__(self, vocab_size: int, embedding_dim: int, pad_idx):
+        super().__init__()
+
+        # embedding(кол-во слов в словаре+паддинг, размерность эмбеддинга, паддинг индекс)
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
+
+        # список сверток(на выходе они будут объеденены для входа в полносвязные слои)
+        self.convs = nn.ModuleList(
+            [
+                nn.Conv2d(
+                    in_channels=1,  # принимает 1 канал
+                    out_channels=64,  # отдает 64 канала
+                    kernel_size=(fs, embedding_dim),  # размер ядра
+                    stride=2  # шаг страйда
+                )
+                for fs in [2, 3, 4]
+            ]
+        )
+        # полносвязные слои
+        self.fc = nn.Linear(3 * 64, 100)  # полносвязный слой из 5*64 входных нейронов и 100 выходных
+        #         self.fc2 = nn.Linear(100, 10)
+        self.fc3 = nn.Linear(100, 1)  # слой из 100 входных и 1 выходного
+
+        self.batchnorm = nn.BatchNorm2d(64)  # слой нормализации
+
+        self.dropout = nn.Dropout(
+            0.5)  # 1/p * x * m 0.5 вероятность выключить нейрон(будем исключать взаимоадаптацию нейронов при обучении)
+
+    def forward(self, x):
+        x = self.embedding(x)  # переводим объект в эмбеддинг
+        x = x.unsqueeze(1)  # добавим размерность(измерение канала) для свертки
+
+        x = [F.relu(conv(x)).squeeze(3) for conv in
+             self.convs]  # пропускаем через свертки через функцию активации и уберем размерность
+
+        x = [F.max_pool1d(_, _.shape[2]).squeeze(2) for _ in x]  # делаем пулинг по длине, убираем ненужную размерность
+
+        x = self.dropout(torch.cat(x, dim=1))  # применяем дропаут
+        x = self.fc(x)
+        x = self.dropout(x)
+        #         x = self.fc2(x)
+        #         x = self.dropout(x)
+
+        return self.fc3(x)
+
