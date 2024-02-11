f19287ba9ad3c5e81451653e6e25c2e050f80834
IldarAkhm <112767933+IldarAkhm@users.noreply.github.com>
2024-02-11 23:18:01 +0300

    modified in Bot.py

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,14 +1,27 @@
 from aiogram import Bot, Dispatcher, executor, types
 from TOKEN import TOKEN_API
+from model import get_model
+import Vocabulary
+import torch
 
 
 bot = Bot(TOKEN_API) # создаем бота с нашим токеном
 dp = Dispatcher(bot) # анализ и инициализая всех входящих апдейтов, функциональность нашего бота
 
+model = get_model() # инициализируем нашу модель
 
-@dp.message_handler()
+voc = Vocabulary() # экземпляр класса словарь
+pad_idx = len(voc.vocabulary) # длина словаря
+
+# @dp.message_handler()
+# async def echo(message: types.Message):
+#     await message.answer(text=message.text) # ответить на сообщение
+
+@dp.message_handler(commands=['start'])
 async def echo(message: types.Message):
-    await message.answer(text=message.text) # ответить на сообщение
+    text_list = voc.encode(message.text)
+    for_model = torch.tensor(text_list + [pad_idx] * (1043 - len(text_list)))
+    await message.answer(text='Иди нахуй, Рамис')
 
 
 if __name__ == '__main__':
